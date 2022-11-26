0a2d9a82beb6589faed770fbd5eb128d0458c85b
NessLeFou <117812901+NessLeFou@users.noreply.github.com>
2022-11-26 23:16:07 +0100

    Add files via upload

--- insert_product_form.php
+++ insert_product_form.php
@@ -54,12 +54,39 @@ include("config.php");
   </head>
   <body>
 
-
+    <br><br>
     <form method="post" class="formulaire2" action="insert_product.php">
+        <p style="text-align:center">Ajouter un produit : <br></p>
         <input type="radio" name="produit" class="checkoption" value="1" required> Machine <br> <!-- Machine -->
         <input type="radio" name="produit" class="checkoption" value="2" required> Filament <br> <!-- Filament -->
         <input type="radio" name="produit" class="checkoption" value="3" required> Accessoire <br> <!-- Accessoire -->
         <p><input type="submit" value="OK"></p>
+    </form>
+
+    <br><br>
+
+    <form method="post" class="formulaire2" action="insert_product_type.php">
+        <p style="text-align:center">Ajouter/Modifier/Supprimer un type de produit : <br></p>
+
+        Machine :
+        <input type="button" value="Ajouter" onclick="window.location.href='http://localhost/ProjetSQL-main/insert_product_type.php?product=1&modif=1';">
+        <input type="button" value="Modifier" onclick="window.location.href='http://localhost/ProjetSQL-main/insert_product_type.php?product=1&modif=2';">
+        <input type="button" value="Supprimer" onclick="window.location.href='http://localhost/ProjetSQL-main/insert_product_type.php?product=1&modif=3';">
+
+        <br><br>
+
+        Filament :
+        <input type="button" value="Ajouter" onclick="window.location.href='http://localhost/ProjetSQL-main/insert_product_type.php?product=2&modif=1';">
+        <input type="button" value="Modifier" onclick="window.location.href='http://localhost/ProjetSQL-main/insert_product_type.php?product=2&modif=2';">
+        <input type="button" value="Supprimer" onclick="window.location.href='http://localhost/ProjetSQL-main/insert_product_type.php?product=2&modif=3';">
+
+        <br><br>
+
+        Accessoire :
+        <input type="button" value="Ajouter" onclick="window.location.href='http://localhost/ProjetSQL-main/insert_product_type.php?product=3&modif=1';">
+        <input type="button" value="Modifier" onclick="window.location.href='http://localhost/ProjetSQL-main/insert_product_type.php?product=3&modif=2';">
+        <input type="button" value="Supprimer" onclick="window.location.href='http://localhost/ProjetSQL-main/insert_product_type.php?product=3&modif=3';">
+
     </form>
 
 
